fc71834b08949b1986d1a055f741279e2d0201d1
Renchester Ramos <renchesterjramos@gmail.com>
2023-03-01 00:05:31 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './sass/main.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
